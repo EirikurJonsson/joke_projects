0d1b75606e980922d3576c613a4fb1862f6b5769
eijo@bunker-holding.com <eijo@bunker-holding.com>
2021-08-13 14:33:10 +0200

    The app works now and this commit can be reverted I want the chuck norris application to work again

--- jokes_project/settings.py
+++ jokes_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     'jokes',
 ]
 
@@ -71,6 +72,9 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'jokes_project.wsgi.application'
+ASGI_APPLICATION = 'jokes_project.asgi.application'
+
+
 
 CELERY_BROKER_URL = 'redis://localhost:6379'
 
@@ -85,6 +89,14 @@ DATABASES = {
     }
 }
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)]
+        }
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
